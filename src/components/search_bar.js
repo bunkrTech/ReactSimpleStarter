5ed5818da8d28330faec92b747dde23e08026299
Blaine Morbitzer <blaine614@gmail.com>
2018-08-10 17:12:13 -0400

    Initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+
+  onInputChange(term) {
+    this.setState({term: term});
+    this.props.onSearchTermChange(term);
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input value={this.state.term}
+          onChange={(event) => {this.onInputChange(event.target.value)}} />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
